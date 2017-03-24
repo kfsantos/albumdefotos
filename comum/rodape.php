b3b78b0e1217fdf675f7f2ba736d1ea008cec8b8
klinger <kfsantos@gmail.com>
2017-03-24 14:09:30 -0400

    Insercao

--- comum/rodape.php
+++ comum/rodape.php
@@ -11,5 +11,6 @@
 <script src = "js/jquery.min.js" ></script >
 <script src = "js/bootstrap.js" ></script >
 </div >
+Rodapé
 </body >
 </html >
--- fotos-por-album.php
+++ fotos-por-album.php
@@ -1,81 +1,35 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: 72512113272
+ * User: kfsantos
  * Date: 21/03/2017
  * Time: 12:19
  */
 require_once("ende-cabecalho.php");
 require_once("ende-banco.php");
-?>
-
-<script type="text/javascript">
-	function drop(url)
-	{
-        document.getElementById("opt").innerHTML = "Deseja apagar o arquivo \""+url+"\" ? <br><br> <button onclick=\"udrop('"+url+"')\">sim</button>&nbsp;<button onclick=\"cancel()\">não</button>"
-		document.getElementById("opt").style.display = 'block';
-	}
-
-	function cancel()
-    {
-        document.getElementById("opt").style.display = 'none';
-    }
-	function udrop(url)
-	{
-        window.location.href = 'delfile.php?file='+url;
-    }
-  </script>
-<?php
 $album = selecionarAlbum($conexao, $_POST['id']);
-$dir = $album['enderecoalbum'];
-
-$exts = array('jpg', 'png', 'jpeg');
-if (is_dir($dir)) {
-
-    if ($d = opendir($dir)) {
-        while (($file = readdir($d)) !== false) {
-            if (filetype($dir . '/' . $file) == 'file') {
-                # recupera a extensao do arquivo
-                $extensao = explode(".", $file);
-                for ($i = 0; $i <= count($exts) - 1; $i++) {
-                    if ($extensao[1] == $exts[$i]) {
-
-                        # Criando o link da imagem pra o lightbox e exibindo a thumb
-                        $alb .= "<a href=\"$dir/{$file}\" rel=\"ex0\" title=\"\"><img border=\"0\" src=\"thumb.php?img=$dir/{$file}\"></a>\n";
-
-                        # Criando o link da imagem pra excluir a imagem
-                        //$alb .=  "<a href=\"javascript:drop('$dir/{$file}')\"> <img src=\"thumb.php?img=$dir/{$file}\" class=\"thumb\"></a>\n";
-
-                    }
-                }
-            }
-        }
-        $onload .= " $(\"a[rel='ex$k']\").colorbox();\n";
+$enderecoalbum = $album['enderecoalbum'];
+$d = opendir($enderecoalbum);
+$i = 0;
+$j = "albuns/TESTE-24-03-2017-16-12-40/";
+$nome = readdir($d);
+while( $nome != false ){
+    if( !is_dir($nome) and ($nome != 'Thumbs.db') and ($nome != 'temporario') ){
+        $arquivos[$i] = $nome;
+                $i++;
     }
-    closedir($d);
-
+    $nome = readdir($d);
 }
+sort($arquivos);
+ 
+foreach($arquivos as $arq){
+    echo '<a href="'.$j.$arq.'" rel="lightbox[roadtrip]"><img src="'.$j.$arq.' " alt="..." /></a> ';
+
+    
+}
+
 ?>
-    <script>
-        $(document).ready(function () {
-            <?=$onload?>
-            $("#click").click(function () {
-                $('#click').css({
-                    "background-color": "#f00",
-                    "color": "#fff",
-                    "cursor": "inherit"
-                }).text("Open this window again and this message will still be here.");
-                return false;
-            });
-        });
-    </script>
 
-<?= $alb ?>
+<?php require_once("ende-rodape.php");
 
 
-
-
-
-<?php
-require_once("ende-rodape.php");
-?>
